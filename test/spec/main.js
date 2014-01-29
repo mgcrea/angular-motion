86149f4f7a910758213b1a9bc7567f3feaf047e3
Olivier Louvignes <olivier@mg-crea.com>
2014-01-29 01:05:19 +0100

    chore(test): update intro test

--- test/spec/main.js
+++ test/spec/main.js
@@ -6,13 +6,13 @@ describe('Animations', function() {
 
   var scope, $animate;
 
-  // Initialize the controller and a mock scope
+  // Load ngAnimate and a mock scope
   beforeEach(inject(function($rootScope, _$animate_) {
     scope = $rootScope.$new();
     $animate = _$animate_;
   }));
 
-  it('should attach a list of awesomeThings to the scope', function() {
+  it('the animate service should be properly defined', function() {
     expect($animate).toBeDefined();
   });
 
